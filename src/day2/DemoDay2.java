6175c23b7958948521a5ad28820fb8d73ea55f00
farah <farah@LAPTOP-EQVP0L72.fios-router.home>
2023-07-29 10:34:26 -0400

    implemented subtract method

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -4,5 +4,12 @@ public class DemoDay2 {
 	public static void main(String[]args) {
 		System.out.println("Test1");
 	}
-
+	//add method
+	public static void add(int a,int b) {
+		System.out.println(a+b);
+	}
+	
+	public static void subtract(int a,int b) {
+		System.out.println(a-b);
+}
 }
