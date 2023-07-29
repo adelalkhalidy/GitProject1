51bdc216bc47ac9f52cac9a2306ab2d68ba3a752
adelalkhalidy <140729981+adelalkhalidy@users.noreply.github.com>
2023-07-29 10:54:18 -0400

    Update DemoDay2.java

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -12,4 +12,8 @@ public class DemoDay2 {
 	public static void subtract(int a,int b) {
 		System.out.println(a-b);
 }
+	//add method
+	public static void add(int a, int b, int c) {
+		System.out.println(a + b + c);
+	}
 }
